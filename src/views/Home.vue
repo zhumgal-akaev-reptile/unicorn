36de272f348ccd3d10008399abdfde44868bf6b4
zhumgal-akaev-reptile <zhumgal.akaev@somo.kg>
2020-12-07 17:28:30 +0600

    implemented countdown unicorn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,421 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="bg">
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      style="display: none;"
+    >
+      <symbol id="wave">
+        <!--        <g style="mix-blend-mode:screen;">-->
+        <!--          <circle-->
+        <!--            r="6.4725"-->
+        <!--            transform="matrix(1 0 0 -1 306.982 252.428)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="6.4725"-->
+        <!--            transform="matrix(1 0 0 -1 249.654 269.996)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="6.4725"-->
+        <!--            transform="matrix(1 0 0 -1 392.049 290.338)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="6.4725"-->
+        <!--            transform="matrix(1 0 0 -1 457.698 252.428)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="6.4725"-->
+        <!--            transform="matrix(1 0 0 -1 510.403 324.55)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="6.4725"-->
+        <!--            transform="matrix(1 0 0 -1 176.607 311.605)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="6.4725"-->
+        <!--            transform="matrix(1 0 0 -1 77.67 324.55)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 58.7148 337.957)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 108.646 309.293)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 154.878 309.293)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 195.562 288.951)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 206.658 309.293)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 219.603 248.267)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 222.377 278.78)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 223.764 290.338)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 205.271 267.222)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 130.375 306.982)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 23.1161 353.214)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 346.741 252.428)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 467.869 289.413)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 504.855 263.523)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 434.582 229.311)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="3.69857"-->
+        <!--            transform="matrix(1 0 0 -1 393.898 275.544)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 273.232 248.267)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 302.821 278.78)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 242.719 299.122)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 342.58 278.78)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 375.867 267.684)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 414.702 276.006)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 428.572 259.362)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 450.764 280.629)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 480.352 270.458)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 490.523 278.78)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 521.961 263.061)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 555.248 265.835)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 544.153 290.8)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 530.283 309.293)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 485.9 347.203)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 480.352 248.267)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 400.833 246.417)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 351.827 227)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--          <circle-->
+        <!--            r="2.31161"-->
+        <!--            transform="matrix(1 0 0 -1 244.568 222.377)"-->
+        <!--            fill="#103C61"-->
+        <!--          />-->
+        <!--        </g>-->
+        <path
+          d="M420,20c21.5-0.4,38.8-2.5,51.1-4.5c13.4-2.2,26.5-5.2,27.3-5.4C514,6.5,518,4.7,528.5,2.7c7.1-1.3,17.9-2.8,31.5-2.7c0,0,0,0,0,0v20H420z"
+        ></path>
+        <path
+          d="M420,20c-21.5-0.4-38.8-2.5-51.1-4.5c-13.4-2.2-26.5-5.2-27.3-5.4C326,6.5,322,4.7,311.5,2.7C304.3,1.4,293.6-0.1,280,0c0,0,0,0,0,0v20H420z"
+        ></path>
+        <path
+          d="M140,20c21.5-0.4,38.8-2.5,51.1-4.5c13.4-2.2,26.5-5.2,27.3-5.4C234,6.5,238,4.7,248.5,2.7c7.1-1.3,17.9-2.8,31.5-2.7c0,0,0,0,0,0v20H140z"
+        ></path>
+        <path
+          d="M140,20c-21.5-0.4-38.8-2.5-51.1-4.5c-13.4-2.2-26.5-5.2-27.3-5.4C46,6.5,42,4.7,31.5,2.7C24.3,1.4,13.6-0.1,0,0c0,0,0,0,0,0l0,20H140z"
+        ></path>
+      </symbol>
+    </svg>
+    <div class="box">
+      <svg
+        width="568"
+        height="380"
+        viewBox="0 0 568 380"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <clipPath id="unicorn">
+          <path
+            d="M563.756 254.682C562.646 251.908 550.718 249.504 547.945 249.041C533.89 246.545 521.592 250.983 490.709 251.445C515.674 213.997 460.936 138.454 486.548 144.464C500.695 147.793 506.428 143.355 515.397 148.625C527.695 155.837 535.924 143.632 538.698 135.495C542.951 123.29 518.079 120.423 510.496 111.732C502.914 103.04 504.486 99.2491 496.349 89.4478C493.206 85.6568 482.757 82.513 478.042 81.2185C479.428 77.7973 501.435 26.2023 511.883 1.51428C512.716 -0.335006 510.404 -0.797328 508.74 1.88414C486.271 36.4658 462.6 72.9892 458.994 78.8145C447.251 55.7908 445.864 73.2666 435.323 70.1228C440.686 73.1741 447.251 71.9721 444.569 80.2014C434.029 72.5269 424.597 59.212 420.066 62.7257C417.292 64.9448 411.282 72.6193 423.673 95.643C399.169 101.838 404.625 106.924 395.378 122.735C402.591 118.204 407.491 116.447 411.19 116.262C381.139 140.858 403.608 154.173 378.735 162.31C381.139 162.495 384.19 162.772 393.159 158.241C381.971 170.447 381.416 183.392 362.831 176.919C362.276 179.323 368.656 183.484 376.701 186.073C363.386 187.368 358.115 193.655 344.8 187.46C350.533 207.062 362.369 198.833 376.701 205.398C354.047 210.946 323.626 207.247 311.328 198.093C266.113 164.251 216.46 166.471 196.395 190.881C187.519 135.957 137.126 136.42 122.054 154.45C61.4898 227.127 54.555 188.385 65.0959 185.518C61.6747 181.172 50.9489 186.535 52.7057 194.117C47.2503 193.378 42.9969 169.152 30.5142 166.286C37.8189 167.95 38.7436 196.429 26.076 184.964C26.3534 211.501 61.5823 218.898 81.0922 212.24C71.9383 218.528 63.3391 220.377 44.2914 215.754C149.053 275.116 155.526 139.379 187.704 210.114C181.231 237.761 196.025 258.842 189.183 270.77C183.635 280.387 162.923 299.157 139.345 281.589C130.93 278.075 115.859 290.095 91.7256 304.057C48.5448 329.023 49.8393 308.958 31.9012 331.704C27.9252 336.697 19.511 337.437 11.8365 340.951C7.39817 342.893 -1.38594 373.406 0.185953 378.214C1.75785 382.93 21.3603 378.214 28.3876 365.176C60.8425 346.314 111.143 314.506 142.951 321.811C212.669 337.807 248.545 324.77 279.798 285.935C327.602 314.136 360.797 309.605 441.056 297.862C463.895 313.027 528.065 282.421 543.136 272.342C546.373 270.216 546.743 272.065 545.263 274.376C517.061 319.499 505.503 302.301 499.216 326.526C496.812 335.588 476.839 336.605 473.696 337.899C470.459 339.194 467.038 373.128 474.62 369.337C479.151 367.118 492.466 362.218 495.61 354.081C520.39 333.276 555.989 297.308 567.085 274.839C568.749 271.325 566.437 261.247 563.756 254.682Z"
+            fill="#103C61"
+          />
+        </clipPath>
+      </svg>
+      <div
+        id="water"
+        class="water"
+        :style="{ transform: `translate(0, ${100 - percentage}%)` }"
+      >
+        <svg viewBox="0 0 560 20" class="water_wave water_wave_back">
+          <use xlink:href="#wave"></use>
+        </svg>
+        <svg viewBox="0 0 560 20" class="water_wave water_wave_front">
+          <use xlink:href="#wave"></use>
+        </svg>
+      </div>
+    </div>
+    <div class="text">
+      <p class="display-2"><span class="amber">1bn</span> COUNTDOWN</p>
+      <h1 class="display-3" style="margin: 20px 0;">
+        {{ total }}
+        <span class="amber">€</span>
+      </h1>
+      <p class="amber display-1">
+        Total <b>issued</b> amount from <b>beginning</b>
+      </p>
+      <p class="display-1" style="margin-top: 20px;">
+        <b class="percentNum" id="count">{{ percentage }}</b>
+        <b class="percentB">%</b>
+      </p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+const BILLION = 1000000000;
+const REFRESH_RATE = 1000;
+const BASE_URL =
+  "https://tableau-dashboards.sunfinance-analytics.com/sales-amount.html";
 
 @Options({
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  total = 0;
+  percentage = 0;
+
+  async created() {
+    const response = await fetch(BASE_URL);
+    this.total = await response.json();
+    this.percentage = Math.floor((this.total / BILLION) * 100);
+    await this.updateStats();
+  }
+
+  updateStats() {
+    let interval = 0;
+    interval = setInterval(async () => {
+      const response = await fetch(BASE_URL);
+      this.total = await response.json();
+      this.percentage = Math.floor((this.total / BILLION) * 100);
+      if (this.total === BILLION) {
+        clearInterval(interval);
+      }
+    }, REFRESH_RATE * 60);
+  }
+
+  mounted() {
+    let interval = 0;
+    interval = setInterval(() => {
+      this.total++;
+      if (this.total === BILLION) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+$waterFColor: #f0bb4c;
+$waterBColor: #ee962f;
+.bubbles {
+  width: 200%;
+  position: absolute;
+  bottom: 100%;
+}
+
+.text {
+  color: white;
+}
+
+.amber {
+  color: $waterFColor;
+}
+
+.display-1 {
+  font-size: 30px;
+}
+
+.display-2 {
+  font-size: 40px;
+}
+
+.display-3 {
+  font-size: 130px;
+}
+
+.bg {
+  background: #154b77 url("../assets/bubble.svg") center no-repeat;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.box {
+  width: 568px;
+  height: 380px;
+  margin: 50px 0;
+  position: relative;
+  overflow: hidden;
+  clip-path: url("#unicorn");
+  background: #103c61;
+
+  .water {
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 2;
+    width: 100%;
+    height: 100%;
+    transform: translate(0, 100%);
+    background: $waterFColor;
+    transition: all 0.3s;
+
+    &_wave {
+      width: 200%;
+      position: absolute;
+      bottom: 100%;
+
+      &_back {
+        right: 0;
+        fill: $waterBColor;
+        animation: wave-back 4s infinite linear;
+      }
+
+      &_front {
+        left: 0;
+        fill: $waterFColor;
+        margin-bottom: -1px;
+        animation: wave-front 2s infinite linear;
+      }
+    }
+  }
+}
+
+@keyframes wave-front {
+  100% {
+    transform: translate(-50%, 0);
+  }
+}
+
+@keyframes wave-back {
+  100% {
+    transform: translate(50%, 0);
+  }
+}
+</style>
